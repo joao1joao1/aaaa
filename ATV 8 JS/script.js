a3ea9ee9a8e1dc0cb6df2a0624ef9bf7ddf51740
joao1joao1 <134083671+joao1joao1@users.noreply.github.com>
2024-04-08 16:59:27 -0300

    do chat :/,
    tinha q ter ensinado function do JS antes Marcio Marçal

---
+++ ATV 8 JS/script.js
@@ -0,0 +1,16 @@
+// const valor1 = document.querySelector("#input1")
+// const valor2 = document.querySelector("#input2")
+
+// const botao = document.querySelector("button")
+
+// const soma = document.querySelector("p")
+
+// valor1.addEventListener("#input1", receberValorSoma)
+// valor2.addEventListener("#input2", receberValorSoma)
+
+// function receberValorSoma(event) {
+//     valor1 = Number(event.target.value)
+//     valor2 = Number(event.target.value)
+
+//     console.log(soma)
+// }
